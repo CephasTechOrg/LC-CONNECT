cc2df997d7b66372209e5e40d76363e77c2573f3
CephasTechOrg <cephasbonsuosei003@gmail.com>
2026-07-31 19:13:02 -0400

    chore: remove Next.js build artifacts and cache files
    This commit deletes various build artifacts and cache files generated by Next.js, including manifest files, build IDs, and cache information. This cleanup helps maintain a clean project structure and ensures that outdated files do not interfere with future builds.

--- employer-portal/.next/static/chunks/app/layout-a77838c3d3d72662.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1076:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,8560,23)),Promise.resolve().then(l.t.bind(l,1290,23))},1290:()=>{},8560:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_8b3a0b"}}},e=>{e.O(0,[600,441,255,358],()=>e(e.s=1076)),_N_E=e.O()}]);
